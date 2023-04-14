f2763f26dfbac78fa8106ba54edafedcb891b333
younu-Yun <dbssnfjd122@gmail.com>
2023-04-14 14:09:48 +0900

    Templates using Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,40 @@
-export const trending = (req, res) => res.send("Homepage videos")
-export const search = (req, res) => res.send("Search videos")
+let videos = [
+    {
+        title: "first videos",
+        rating: 5,
+        comments: 2,
+        createAt: "2 min ago",
+        view: 59,
+        id: 1,
+    },
+    {
+        title: "second videos",
+        rating: 5,
+        comments: 2,
+        createAt: "2 min ago",
+        view: 59,
+        id: 2,
+    },
+    {
+        title: "third videos",
+        rating: 5,
+        comments: 2,
+        createAt: "2 min ago",
+        view: 59,
+        id: 3,
+    },
+];
 
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const search = (req, res) => res.send("Search videos");
 
-export const see = (req, res) => res.send("see video")
-export const edit = (req, res) => res.send("edit video")
-export const upload = (req, res) => res.send("upload video")
-export const remove = (req, res) => res.send("delete video")
-
+export const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const upload = (req, res) => res.send("upload video");
+export const remove = (req, res) => res.send("delete video");
--- src/server.js
+++ src/server.js
@@ -1,24 +1,28 @@
 import express from "express";
 import morgan from "morgan";
 
-import globalRouter from '../router/globalRouter'
-import userRouter from '../router/userRouter'
-import videoRouter from '../router/videoRouter'
+import globalRouter from "../router/globalRouter";
+import userRouter from "../router/userRouter";
+import videoRouter from "../router/videoRouter";
 
 const app = express();
 const PORT = 4000;
-const logger = morgan('dev')
-app.use(logger)
-
+const logger = morgan("dev");
+app.use(logger);
 
 /* -------------------------------------------- */
-app.use('/', globalRouter)
-app.use('/users', userRouter)
-app.use('/videos', videoRouter)
+//express의 html view engine을 Pug로 설정
+app.set("view engine", "pug");
+//package.json이 있는 위치를 내가 원하는곳을 바꿔줌
+app.set("views", `${process.cwd()}/src/views`);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 /* -------------------------------------------- */
 const handleListening = () => {
-    console.log(`✨Server listening on port 🔗http://localhost:${PORT}`)
-}
+    console.log(`✨Server listening on port 🔗http://localhost:${PORT}`);
+};
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
